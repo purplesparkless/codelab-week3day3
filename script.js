0fb080125cf0578b1aab6e38abd113347d8bb881
Zainab Sansa <tunrayotemitope05@gmail.com>
2024-01-29 02:48:59 +0100

    new commit

---
+++ script.js
@@ -0,0 +1,16 @@
+//TYPE CONVERSION AND TYPE COERCION
+const year = '1981';
+console.log(year);
+console.log(Number(year));
+
+const today ='2024';
+console.log(String(today));
+
+console.log('I am ' + 30 + 'years old') //converts the number in concatenation to a string
+
+//TRUITHY AND FALSEY
+// LOGICAL OPERATORS
+//5 FALSEY VALUES: 0, '', UNDEFINED, NaN, FALSE
+//5 TRUITY VALUES: 1, 'HGDVHGJV', TRUE
+
+console.log(boolean(0));
